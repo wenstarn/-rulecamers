2595d4a63379ad0811889368cfc7bab6d903d3b4
wenstarn <34746081+wenstarn@users.noreply.github.com>
2020-11-16 00:25:37 +0300

    Create client.py

---
+++ client.py
@@ -0,0 +1,37 @@
+import sys
+import socket
+import pickle
+
+client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_sock.connect(('127.0.0.1', int(sys.argv[1])))
+while True:
+    print((client_sock.recv(2048)).decode())
+    a = input()
+    b = input()
+    c = input()
+    d = input()
+    l = [a,b,c,d]
+    data = pickle.dumps(l)
+    client_sock.sendall(data)
+    
+    message = client_sock.recv(2048).decode()
+    print(message)
+    
+    if message ==  'Успешное подключение':
+        break
+
+while True:
+    message = client_sock.recv(2048).decode()
+    if message == "exit":
+        break
+    
+    elif message == 'Неверная команда':
+        print(message)
+        continue
+        
+    else:
+        print(message)
+        data = input()
+        client_sock.sendall(data.encode())
+    
+client_sock.close()
