ffe05e05b0f47c63f40b0ee7c507200baa7595ea
wenstarn <34746081+wenstarn@users.noreply.github.com>
2020-11-26 02:11:30 +0300

    Update server.py

--- server.py
+++ server.py
@@ -2,6 +2,14 @@ import socket
 import sys
 import pickle
 from camera import ptzcam
+import datetime
+
+time_format = "%Y-%m-%d %H:%M:%S"
+
+def add_in_log(string):
+    log_file = open('log.txt', 'a', encoding='utf-8')
+    log_file.write(string)
+    log_file.close()
 
 def is_digit(string):
     if string.isdigit():
@@ -12,21 +20,26 @@ def is_digit(string):
             return True
         except ValueError:
             return False
-
+        
+def get_time():
+    now = datetime.datetime.now(datetime.timezone.utc).astimezone()
+    return f"{now:{time_format}}"
 def run_server(port):
     serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serv_sock.bind(("127.0.0.1", port))
     serv_sock.listen(5)
     while True:
         client_sock, client_address = serv_sock.accept()
-        print ('Подключился', client_address)
+        string = get_time() + ' Connected ' + str(client_address) + '\n'
+        print(string)
+        add_in_log(string)
         while True:
-            client_sock.sendall('Введите IP камеры, ONVIF порт, логин и пароль'.encode())
+            client_sock.sendall('Enter IP, ONVIF port, username and password'.encode())
             if serve_client(client_sock, client_address , port):
                 break
             else:
-                client_sock.sendall('Неудачное подключение'.encode())
-            
+                client_sock.sendall('Invalid command'.encode())
+           
 def serve_client(client_sock, client_address, port):
     try:
             data = client_sock.recv(2048)
@@ -35,16 +48,22 @@ def serve_client(client_sock, client_address, port):
     except:
             data = None
     if data == None:
-        print ('Произошло внезапное отключение', client_address)
+        string = get_time() + f" User {client_address} unexpectedly disconnected\n"
+        print (string)
+        add_in_log(string)
+        client_sock.close()
+        return True
     else:
-        obj = pickle.loads(data)
-        camera = ptzcam(obj[0], obj[1], obj[2], obj[3])
-        if camera.mycam == None:
+        camera = handle_data(data, client_address)
+        if not camera.mycam:
             return False
         else: 
-            client_sock.sendall('Успешное подключение'.encode())
+            client_sock.sendall('Successful connection'.encode())
+            string = get_time() + f" User {client_address} successfully connected to camera ({camera.ip}, {camera.port})\n"
+            print(string)
+            add_in_log(string)
             while True:
-                client_sock.sendall('Введите команду или exit, чтобы отключиться'.encode())
+                client_sock.sendall('Enter command or exit to disconnect'.encode())
                 try:
                     chunk = client_sock.recv(2048)
                     if not chunk:
@@ -52,32 +71,48 @@ def serve_client(client_sock, client_address, port):
                 except:
                     chunk = None
                 if chunk == None:
-                    print ('Произошло внезапное отключение', client_address)
-                    break
+                    string = get_time() + f" User {client_address} unexpectedly disconnected from camera ({camera.ip}, {camera.port})\n"
+                    print (string)
+                    add_in_log(string)
+                    client_sock.close()
+                    return True
                 else:
-                    chunk = chunk.decode()
-                    chunk  = chunk.split()
-                    if len(chunk) == 1 and chunk[0] == "exit":
-                        client_sock.sendall("exit".encode())
-                        break
-                    elif  len(chunk) == 3 and is_digit(chunk[1]) and is_digit(chunk[2]):
-                        
-                        if  chunk[0] == "tilt":
-                            camera.move_tilt(float(chunk[1]), float(chunk[2]))
-                            
-                        elif  chunk[0] == "pan":
-                            camera.move_pan(float(chunk[1]), float(chunk[2]))
-                            
-                        elif chunk[0] == "zoom":
-                            camera.zoom(float(chunk[1]), float(chunk[2]))
-                            
-                        else:
-                            client_sock.sendall('Неверная команда'.encode())
-                    else:
-                       client_sock.sendall('Неверная команда'.encode())
-                       
+                    if handle_commands(chunk, camera, client_sock):
+                        break    
     client_sock.close()
-    print ('Отключился', client_address)
+    string = get_time() + f" User {client_address} finished work camera with ({camera.ip}, {camera.port})\n"
+    print (string)
+    add_in_log(string)
     return True
+
+def handle_data(data, client_address):
+    obj = pickle.loads(data)
+    camera = ptzcam(obj[0], obj[1], obj[2], obj[3], client_address)
+    return camera
+
+def handle_commands(chunk, camera, client_sock):
+    chunk = chunk.decode()
+    chunk  = chunk.split()
+    if len(chunk) == 1 and chunk[0] == "exit":
+        client_sock.sendall("exit".encode())
+        return True
+    elif  len(chunk) == 3 and is_digit(chunk[1]) and is_digit(chunk[2]):
+                        
+        if  chunk[0] == "tilt":
+            camera.move_tilt(float(chunk[1]), float(chunk[2]))
+                            
+        elif  chunk[0] == "pan":
+            camera.move_pan(float(chunk[1]), float(chunk[2]))
+                            
+        elif chunk[0] == "zoom":
+            camera.zoom(float(chunk[1]), float(chunk[2]))
+                            
+        else:
+            client_sock.sendall('Invalid command'.encode())
+    else:
+        client_sock.sendall('Invalid command'.encode())
+        
+    return False
+    
 if __name__ == '__main__':
     run_server(port=int(sys.argv[1]))
